04cabd88c1866d9863b6743608c52727ee31bd21
Philipp Meier <philipp@meier.name>
2012-02-10 20:54:25 +0100

    Bump version number to avoid confusion on clojars

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject compojure-rest "0.2"
+(defproject compojure-rest "0.2.2"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [compojure "1.0.1"]
 		 [commons-codec/commons-codec "1.5"]
-                 [org.clojure/tools.trace "0.7.1"]
-                 ]
-  :dev-dependencies [
-                     [swank-clojure "1.3.3"]
+                 [org.clojure/tools.trace "0.7.1"]]
+  :dev-dependencies [[swank-clojure "1.3.3"]
                      [malcolmsparks/ring-mock "0.2.0"]
                      [ring/ring-jetty-adapter "1.0.1"]
-                     [hiccup "0.3.7"]
-		     ])
+                     [hiccup "0.3.7"]])
