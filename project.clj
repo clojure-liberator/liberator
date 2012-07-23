e9181b80cb14e03c5ae7872e1b8c14d5e45ffd68
Malcolm Sparks <malcolm@congreve.com>
2012-07-23 19:26:29 +0100

    Move to lein2 and testing with Midje.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 ;; Use leiningen 2
 (defproject liberator "0.3.1-SNAPSHOT"
   :description "Liberator - A REST library for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.trace "0.7.3"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.2"]
@@ -9,13 +9,26 @@
                  [org.clojure/clojurescript "0.0-1236"]
                  [hiccup "1.0.0"] ;; Used by code rendering default representations.
                  ] 
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]
+            [lein-ring "0.7.1"]]
 
   :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.1.0"]
                                   [ring-mock "0.1.2"]
+                                  [com.stuartsierra/lazytest "1.2.3"]
                                   [compojure "1.0.2"] ;; only for examples
-                                  [midje "1.3.1"] ;; can we remove?
+                                  [midje "1.4.0"]
                                   ]}}
-  :source-paths ["src"]
-  :test-paths ["test"])
+
+  ;; This is only needed for lazytest
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}
+  
+  :source-paths ["src" "examples/clj"]
+  :test-paths ["test"]
+
+  :ring {:handler examples.server/handler
+         :adapter {:port 8000}}
+
+  :aliases {"examples" ["run" "-m" "examples.server"]}
+  
+  )
 
