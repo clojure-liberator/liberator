8986b247c54f0c7a6c267c680e0c13d1719bb918
Philipp Meier <github@philipp.meier.name>
2012-09-06 15:52:27 +0200

    Finalize test of get/put/delete on resource
    * Delete is expected to return NO-CONTENT if enacted
    * Add NO-CONTENT to checkers

--- test/test_get_put.clj
+++ test/test_get_put.clj
@@ -2,35 +2,36 @@
   (:use liberator.core
         midje.sweet
         checkers
-        [ring.mock.request :only [request header]]
-        [clojure.tools.trace :only [trace]]))
+        [ring.mock.request :only [request header]]))
 
 ;; tests for a simple resource that can be accessed on a given uri
+
+
 
 (defn make-thing-resource [things]
   (resource 
    :method-allowed? (request-method-in :get :put :delete)
+   ;; are resource exists if a value is stored in @things at the uri 
    :exists?  #(not (nil? (get @things (get-in % [:request :uri]))))
-   :existed? #(nil? (get @things (get-in % [:request :uri])))
+   ;; ...and id existed if the stored value is nil (and not some random Object. used as setinel
+   :existed? #(and (nil? (get @things (get-in % [:request :uri]) (Object.))))
    :handle-ok #(get-in @things [(get-in % [:request :uri]) :content])
-   :x-known-content-type? #(= "text/plain" (get-in % [:request :headers "content-type"]))
    :put! #(dosync (alter things assoc-in [(get-in % [:request :uri]) :content] (get-in % [:request :body])))
    :delete! #(dosync (alter things assoc (get-in % [:request :uri]) nil))))
 
-(with-console-logger
-  (let [things (ref nil)
-        r (make-thing-resource things)]
-    (do
-      (let [resp (r (request :get "/r1"))]
-        (fact "get => 404" resp => NOT-FOUND))
-      (let [resp (r (-> (request :put "/r1")
-                        (assoc :body "r1")
-                        (header "content-type" "text/plain")))]
-        (fact "put => 202" resp => CREATED))
-      (let [resp (r (-> (request :get "/r1")))]
-        (fact "get => 200" resp => OK)
-        (fact "get body is what was put before" resp => (body "r1")))
-      (let [resp (r (-> (request :delete "/r1")))]
-        (fact "delete" resp => ACCEPTED))
-      (let [resp (r (request :get "/r1"))]
-        (fact "get => gone" resp => GONE)))))
+(let [things (ref nil)
+      r (make-thing-resource things)]
+  (do
+    (let [resp (r (request :get "/r1"))]
+      (fact "get => 404" resp => NOT-FOUND))
+    (let [resp (r (-> (request :put "/r1")
+                      (assoc :body "r1")
+                      (header "content-type" "text/plain")))]
+      (fact "put => 202" resp => CREATED))
+    (let [resp (r (-> (request :get "/r1")))]
+      (fact "get => 200" resp => OK)
+      (fact "get body is what was put before" resp => (body "r1")))
+    (let [resp (r (-> (request :delete "/r1")))]
+      (fact "delete" resp => NO-CONTENT))
+    (let [resp (r (request :get "/r1"))]
+      (fact "get => gone" resp => GONE))))
