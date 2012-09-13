f722bbe29eca2d958930237ab964c52a5a658a50
Philipp Meier <github@philipp.meier.name>
2012-09-13 13:37:27 +0200

    Colorize handlers in flow graph according to http status class

--- test/doc.clj
+++ test/doc.clj
@@ -4,24 +4,29 @@
   ([_ name then else] [name then else])
   ([_ name test then else] [name then else]))
 
-(defn format-node [node]
-  (when (.startsWith (str node) "handle-")
-    (format "\"%s\" [peripheries=2];\n" node)))
-
 (defn to-graph [[& args]]
   (condp = (first args)
     'defdecision
     (let [[name then else] (apply destructure args)]
-      (str
-       (format-node then)
-       (format-node else)
-       (format (str "\"%s\" -> \"%s\" [label = \"true\"] \n"
-                    "\"%s\" -> \"%s\" [label=\"false\"]\n")
-               name then name else)))
+      (format (str "\"%s\" -> \"%s\" [label = \"true\"] \n"
+                   "\"%s\" -> \"%s\" [label=\"false\"]\n")
+              name then name else))
     'defaction
     (let [[_ name then] args]
       (format "\"%s\"[shape=\"ellipse\"];\n\"%s\"-> \"%s\"\n" name name then))
-    nil))
+    'defhandler
+    (let [[_ name status message] args
+          color (cond
+                 (>= status 500) "#e31a1c"
+                 (>= status 400) "#fb9a99"
+                 (>= status 300) "#fbdf6f"
+                 (>= status 200) "#b2df8a"
+                 (>= status 100) "#a6cee3"
+                 :else "#ffffff")]
+      (format "\"%s\"[label=\"%s\\n%s\" style=\"filled\" fillcolor=\"%s\"];\n" name status name color))
+    nil)
+  
+  )
 
 (defn rank-max [names]
   (str "subgraph {\nrank=max;\n"
