06f46116ce54de425cebf206edd3ea19bd08c5ae
Philipp Meier <philipp@meier.name>
2013-02-10 21:31:07 +0100

    Simplify test using tabular

--- test/test_representation.clj
+++ test/test_representation.clj
@@ -6,13 +6,14 @@
 ;; test for issue #19
 ;; https://github.com/clojure-liberator/liberator/pull/19
 
-(facts "Can produce csv from map"
-       (fact
-        (as-response (->MapRepresentation {:foo "foo" :bar "bar"}) {:representation {:media-type "text/csv"}})
-        => {:body "name,value\r\n:foo,foo\r\n:bar,bar\r\n"}))
+(facts "Can produce representations from map"
+       (tabular "Various media types are supported"
+                (as-response (->MapRepresentation {:foo "bar" :baz "qux"}) {:representation {:media-type ?media-type}})
+                => {:body ?body}
+                ?media-type   ?body
+                "text/csv"    "name,value\r\n:foo,bar\r\n:baz,qux\r\n"
+                "text/tab-separated-values" "name\tvalue\r\n:foo\tbar\r\n:baz\tqux\r\n"
+                "text/plain"  "foo=bar\r\nbaz=qux"))
 
-(facts "Can produce tsv from map"
-       (fact
-        (as-response (->MapRepresentation {:foo "foo" :bar "bar"}) {:representation {:media-type "text/tab-separated-values"}})
-        => {:body "name\tvalue\r\n:foo\tfoo\r\n:bar\tbar\r\n"}))
+
 
