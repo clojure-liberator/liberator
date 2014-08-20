73e247bc9f6647da53ac4aa6067bf1132b21c715
Philipp Meier <philipp@meier.name>
2014-08-20 14:35:35 +0200

    lein-release plugin: bumped version from 0.12.1 to 0.12.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liberator "0.12.1"
+(defproject liberator "0.12.2-SNAPSHOT"
   :description "Liberator - A REST library for Clojure."
   :url "http://clojure-liberator.github.io/liberator"
   :dependencies [[org.clojure/clojure "1.4.0"]
@@ -6,6 +6,7 @@
                  [org.clojure/data.csv "0.1.2"]
                  [hiccup "1.0.3"]] ;; Used by code rendering default representations.
   :deploy-repositories  [["releases" :clojars]]
+  :lein-release {:deploy-via :clojars}
 
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
