83e8f851dbc49f16669efe08a67c694b35b2bf5f
Philipp Meier <philipp@meier.name>
2014-09-24 09:44:57 -0400

    lein-release plugin: preparing 0.12.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liberator "0.12.2-SNAPSHOT"
+(defproject liberator "0.12.2"
   :description "Liberator - A REST library for Clojure."
   :url "http://clojure-liberator.github.io/liberator"
   :dependencies [[org.clojure/clojure "1.4.0"]
