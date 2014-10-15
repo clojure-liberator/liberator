23f7648cb8c17faba21d3d7c4c2deb59c3fa8966
siscia <simone@mweb.biz>
2014-10-15 09:46:23 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject liberator "0.12.0"
   :description "Liberator - A REST library for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.1"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/data.csv "0.1.2"]
-                 [hiccup "1.0.3"]] ;; Used by code rendering default representations.
+                 [hiccup "1.0.5"]] ;; Used by code rendering default representations.
 
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
@@ -16,17 +16,17 @@
   :plugins [[lein-midje "3.1.3" :exclusions [leiningen-core]]
             [lein-ring "0.8.10" :exclusions [org.clojure/clojure]]]
 
-  :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.2.1" :exclusions [joda-time]]
-                                  [ring-mock "0.1.2"]
-                                  [ring/ring-devel "1.2.1" :exclusions [joda-time]]
-                                  [midje "1.6.0" :exclusions [org.clojure/clojure]]
+  :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.3.1" :exclusions [joda-time]]
+                                  [ring-mock "0.1.5"]
+                                  [ring/ring-devel "1.3.1" :exclusions [joda-time]]
+                                  [midje "1.6.3" :exclusions [org.clojure/clojure]]
                                   ;; only for examples
-                                  [compojure "1.0.2" :exclusions [org.clojure/tools.macro]]
+                                  [compojure "1.2.0" :exclusions [org.clojure/tools.macro]]
                                   [org.clojure/clojurescript "0.0-1450"]]
                    :source-paths [ "src" "examples/clj"]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
 
   :source-paths ["src"]
   :test-paths ["test"]
