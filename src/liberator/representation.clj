eb3e7e41591d798313e17bfda81eeace4c6ba06d
Andrew Mcveigh <me@andrewmcveigh.com>
2014-12-20 10:40:42 +0100

    Remove now redundant MultiFn as-response

--- src/liberator/representation.clj
+++ src/liberator/representation.clj
@@ -201,10 +201,6 @@
   (as-response [this context]
     (as-response (render-map-generic this context) context))
 
-  clojure.lang.MultiFn
-  (as-response [multi-fn context]
-    (as-response (multi-fn context) context))
-
   ;; If a string is returned, we should carry out the conversion of both the charset and the encoding.
   String
   (as-response [this {representation :representation}]
