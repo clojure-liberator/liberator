82663ee604fcc1863335c3f456c7acaba3a93766
Philipp Meier <philipp@meier.name>
2015-11-13 15:39:45 +0100

    Version 0.14.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liberator "0.14.0-SNAPSHOT"
+(defproject liberator "0.14.0"
   :description "Liberator - A REST library for Clojure."
   :url "http://clojure-liberator.github.io/liberator"
   :dependencies [[org.clojure/clojure "1.4.0"]
